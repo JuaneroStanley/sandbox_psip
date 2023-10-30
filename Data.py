ea57f69c86bc1ab36edd144379e019d557a57552
JuaneroStanley <stanislawskrzynskijan@gmail.com>
2023-10-30 14:44:02 +0100

    first User data

---
+++ Data.py
@@ -0,0 +1,8 @@
+user_data: list[str,str,int] = [{"name":"Stanislaw","nick":"Juanero","posts":42},
+             {"name":"Oliver","nick":"Szymannski","posts":12},
+             {"name":"Bartosz","nick":"Baran","posts":123},
+             {"name":"Tomasz","nick":"Koc","posts":34},
+             {"name":"Marek","nick":"WEIS","posts":555},
+             {"name":"Piotr","nick":"Suchy","posts":42},
+             {"name":"Szymon","nick":"Kępka","posts":12}
+]
