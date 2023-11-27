2f97e73926b8fbe6a9a93b7a82e15bba52d78ca8
JuaneroStanley <stanislawskrzynskijan@gmail.com>
2023-11-27 13:55:15 +0100

    chore generating map

--- newRandom.py
+++ newRandom.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import folium
 
 def get_coordinates_of(city:str)->list[float,float]:
     adress_url = f'https://pl.wikipedia.org/wiki/{city}'
@@ -8,3 +8,13 @@ def get_coordinates_of(city:str)->list[float,float]:
     latitude = float(BeautifulSoup(response.text, 'html.parser').find_all('span', class_='latitude')[1].text.replace(',', '.')) # .latitude is same as class_='latitude'
     longitude = float(BeautifulSoup(response.text, 'html.parser').select('.longitude')[1].text.replace(',','.')) # .longitude is same as class_='longitude' 
     return [latitude,longitude]
+cities = ["Warszawa","Kraków","Gdańsk","Bydgoszcz"]
+
+map = folium.Map(location=get_coordinates_of("Warszawa"),
+                 tiles="OpenStreetMap", 
+                 zoom_start=6)
+for city in cities:
+    folium.Marker(location=get_coordinates_of(city), 
+              popup="My tu a ty??????"
+              ).add_to(map)
+map.save("mapka.html")
--- utils/my_functions.py
+++ utils/my_functions.py
@@ -191,9 +191,3 @@ def ui():
             case "6": coordinates_city()
             case "0": ui_exit = True
 
-def get_coordinates_of(city:str)->list[float,float]:
-    adress_url = f'https://pl.wikipedia.org/wiki/{city}'
-    response = requests.get(url=adress_url)
-    latitude = float(BeautifulSoup(response.text, 'html.parser').find_all('span', class_='latitude')[1].text.replace(',', '.')) # .latitude is same as class_='latitude'
-    longitude = float(BeautifulSoup(response.text, 'html.parser').select('.longitude')[1].text.replace(',','.')) # .longitude is same as class_='longitude' 
-    return [latitude,longitude]
